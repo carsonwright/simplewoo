c9efd9c1a8889822912025e2bedff46484632005
Jason Truluck <jason.truluck@gmail.com>
2013-10-28 18:34:59 -0400

    Add rspec
    add rspec and spec helper files for testing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'coveralls'
+Coveralls.wear!
+
+ENV["RAILS_ENV"] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+class SimpleCov::Formatter::QualityFormatter
+  def format(result)
+    SimpleCov::Formatter::HTMLFormatter.new.format(result)
+    File.open("coverage/covered_percent", "w") do |f|
+      f.puts result.source_files.covered_percent.to_f
+    end
+  end
+end
+SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
+
+require 'simplewoo'
+require "webmock/rspec"
+
+Dir[File.expand_path("spec/support/**/*.rb", __FILE__)].each {|f| require f}
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.color_enabled = true
+  config.order                                           = "random"
+end
+
+def stub_woo(http_method = :any, endpoint = "/", status = 200, content_type = "application/json", response)
+  stub_request(http_method, "https://api.woofound.com#{endpoint}").
+  to_return(:status => status, 
+            :body => File.read(File.expand_path("../support/mocks/#{response}.json", __FILE__)),
+            :headers =>{'Accept' => content_type, 'Content-type' => content_type})
+end
