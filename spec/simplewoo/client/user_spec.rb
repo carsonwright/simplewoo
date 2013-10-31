d5d96460245e8de586f48612a9abaad4648b677d
Jason Truluck <jason.truluck@gmail.com>
2013-10-31 17:08:33 -0400

    add the me method for the user
    This updates authentication to allow the user to auth via email +
    password or authe token
    this will return the currently authenticated user

--- spec/simplewoo/client/user_spec.rb
+++ spec/simplewoo/client/user_spec.rb
@@ -11,20 +11,56 @@ describe Simplewoo::Client do
     end
   end
 
-  context "valid" do
-    let(:client) { Simplewoo::Client.new(:email => "user@example.com", :password => "somepassword") }
+  describe ".create_user" do
+    let(:client) { Simplewoo::Client.new }
 
-    describe ".create_user" do
+    context "valid" do
       before(:each) do
-        stub_woo(:post, "/users?user%5Bemail%5D=jason@example.com&user%5Bfirst_name%5D=Truluck&user%5Blast_name%5D%5Bbio%5D=Some%20awesome%20bio.&user%5Blast_name%5D%5Bbirthday%5D=1988-01-01&user%5Bpassword%5D=password&user%5Bpassword_confirmation%5D=password", "user")
+        stub_woo(:post, "/users?user%5Bbirthday%5D=Jason&user%5Bemail%5D=jason@example.com&user%5Bfirst_name%5D=Truluck&user%5Blast_name%5D%5Bbio%5D=Some%20awesome%20bio.&user%5Blast_name%5D%5Bbirthday%5D=1988-01-01&user%5Bpassword%5D=password&user%5Bpassword_confirmation%5D=password", "user")
+        stub_woo(:get, "/users/me", 200, "jason@example.com:password@", "me")
       end
 
       let(:response) {
-        client.create_user("jason@example.com", "password", "password", "Jason", "Truluck", { :bio      => "Some awesome bio.",:birthday => "1988-01-01"})
+        client.create_user("jason@example.com", "password", "password", "Jason", "Truluck", { :bio => "Some awesome bio.", :birthday => "1988-01-01"})
       }
 
       it "returns the users email" do
         expect(response).to respond_to(:email)
+      end
+
+      it "queries the Core API" do
+        expect(WebMock).to have_requested(:get, "https://jason@example.com:password@api.woofound.com/users/me").once
+      end
+
+      it "sets the api_token" do
+        expect(client.api_token).to eq("acce$$token") 
+      end
+    end
+  end
+
+  describe ".me" do
+    context "authenticated" do
+      let(:client) { Simplewoo::Client.new(:api_token => "some_token") }
+
+      before(:each) do
+        stub_woo(:get, "/users/me", 200, ":some_token@", "me")
+      end
+
+      it "returns the authenticated user" do
+        expect(client.me).to_not be_nil
+      end
+
+      it "queries the Core API" do
+        client.me
+        expect(WebMock).to have_requested(:get, "https://:some_token@api.woofound.com/users/me").once
+      end
+    end
+
+    context "unauthenticated" do
+      let(:client) { Simplewoo::Client.new }
+
+      it "raises a Simplewoo::Unauthorized error" do
+        expect{ client.me }.to raise_error("No authorized user.")
       end
     end
   end
