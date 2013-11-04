5448238230b06cbfaa852730629b2104d443059e
Jason Truluck <jason.truluck@gmail.com>
2013-11-04 15:29:56 -0500

    add user endpoints and specs

--- lib/simplewoo/client/user.rb
+++ lib/simplewoo/client/user.rb
@@ -1,7 +1,7 @@
 module Simplewoo
   class Client
     module User
-      # Create a user on the core api 
+      # Create a user on the core api
       #
       # @param [String] email - the email of the user
       # @param [String] password - the password of the user
@@ -15,12 +15,14 @@ module Simplewoo
       #
       # @return [Hashie::Mash] user the user created by the request
       #
-      # @example Create a user 
+      # @example Create a user
       #   Simplewoo.create_user({
       #     :email => "some@example.com",
       #     :password => "password",
       #     :password_confirmation => "password",
-      #     :birthday => "1988-01-01"
+      #     :birthday => "1988-01-01",
+      #     :first_name => "Johnny",
+      #     :last_name => "Test"
       #   })
       def create_user(email, password, password_confirmation, birthday, first_name, last_name, options = {})
         options.merge!({
@@ -29,7 +31,7 @@ module Simplewoo
           :password_confirmation => password_confirmation,
           :birthday              => birthday,
           :first_name            => first_name,
-          :last_name             => last_name,
+          :last_name             => last_name
         })
 
         result = post("/users", { :user => options })
@@ -38,13 +40,41 @@ module Simplewoo
         result
       end
 
-      # Returns the currently authenticated user
-      # 
+      # Update a user on the core api
+      #
       # @note This method requires an authenticated user via email + password or api_token
-      # 
+      #
+      # @option options [String] email - the email of the user
+      # @option options [String] password - the password of the user
+      # @option options [String] password_confirmation - the password_confirmation of the user
+      # @option options [String] birthday - the birthday of the user
+      # @option options [String] first_name - the first name of the user
+      # @option options [String] last_name - the first name of the user
+      # @option options [String] bio - The users bio
+      # @option options [String] image - The users image
+      #
+      # @return [Hashie::Mash] user the user updated by the request
+      #
+      # @example Update a user
+      #   Simplewoo.update_user({
+      #     :email => "some_new_email@example.com"
+      #   })
+      def update_user(options = {})
+        if authenticated?
+          put("/users/me", { :user => options })
+        else
+          raise "No authorized user."
+        end
+      end
+      alias :reset_password :update_user
+
+      # Returns the currently authenticated user
+      #
+      # @note This method requires an authenticated user via email + password or api_token
+      #
       # @example Return the user
       #  Simplewoo.me
-      def me 
+      def me
         if authenticated?
           get("/users/me")
         else
--- spec/simplewoo/client/user_spec.rb
+++ spec/simplewoo/client/user_spec.rb
@@ -15,14 +15,18 @@ describe Simplewoo::Client do
     let(:client) { Simplewoo::Client.new }
 
     context "valid" do
-      before(:each) do
-        stub_woo(:post, "/users?user%5Bbirthday%5D=Jason&user%5Bemail%5D=jason@example.com&user%5Bfirst_name%5D=Truluck&user%5Blast_name%5D%5Bbio%5D=Some%20awesome%20bio.&user%5Blast_name%5D%5Bbirthday%5D=1988-01-01&user%5Bpassword%5D=password&user%5Bpassword_confirmation%5D=password", "user")
-        stub_woo(:get, "/users/me", 200, "jason@example.com:password@", "me")
-      end
-
       let(:response) {
         client.create_user("jason@example.com", "password", "password", "Jason", "Truluck", { :bio => "Some awesome bio.", :birthday => "1988-01-01"})
       }
+
+      before(:each) do
+        stub_woo(:post, "/users?user%5Bbirthday%5D=Jason&user%5Bemail%5D=jason@example.com&user%5Bfirst_name%5D=Truluck&user%5Blast_name%5D%5Bbio%5D=Some%20awesome%20bio.&user%5Blast_name%5D%5Bbirthday%5D=1988-01-01&user%5Bpassword%5D=password&user%5Bpassword_confirmation%5D=password", "user")
+        stub_woo(:get, "/users/me", 200, "jason@example.com:password@", "me")
+
+        # Initializes the user for the client
+        response
+      end
+
 
       it "returns the users email" do
         expect(response).to respond_to(:email)
@@ -47,12 +51,7 @@ describe Simplewoo::Client do
       end
 
       it "returns the authenticated user" do
-        expect(client.me).to_not be_nil
-      end
-
-      it "queries the Core API" do
-        client.me
-        expect(WebMock).to have_requested(:get, "https://:some_token@api.woofound.com/users/me").once
+        expect(client.me).to respond_to(:email)
       end
     end
 
@@ -61,6 +60,51 @@ describe Simplewoo::Client do
 
       it "raises a Simplewoo::Unauthorized error" do
         expect{ client.me }.to raise_error("No authorized user.")
+      end
+    end
+  end
+
+  describe ".update_user" do
+    let(:response) { client.update_user({:email => "tom@example.com"}) }
+
+    context "authenticated" do
+      let(:client) { Simplewoo::Client.new(:api_token => "some_token") }
+
+      it "updates user email field" do
+        stub_woo(:put, "/users/me?user%5Bemail%5D=tom@example.com", 200, ":some_token@", "updated_user")
+
+        expect(response.email).to eq("tom@example.com")
+      end
+    end
+
+    context "unauthenticated" do
+      let(:client) { Simplewoo::Client.new }
+
+      it "raises a Simplewoo::Unauthorized error" do
+        stub_woo(:put, "/users/me?user%5Bemail%5D=tom@example.com", "updated_user")
+
+        expect{ response }.to raise_error("No authorized user.")
+      end
+    end
+  end
+
+  describe ".reset_password" do
+    let(:response) { client.update_user({:password => "awesome_password", :password_confirmation => "awesome_password" }) }
+
+    context "authenticated" do
+      let(:client) { Simplewoo::Client.new(:api_token => "some_token") }
+
+      # PENDING Update the core to revalidate the access token on successful password change
+      it "updates user password"
+    end
+
+    context "unauthenticated" do
+      let(:client) { Simplewoo::Client.new }
+
+      it "raises a Simplewoo::Unauthorized error" do
+        stub_woo(:put, "/users/me?user%5Bpassword%5D=awesome_password%5Bpassword_confirmation%5D=awesome_password", "updated_user")
+
+        expect{ response }.to raise_error("No authorized user.")
       end
     end
   end
